65f8a1d34a2af5905c720abe41eb92da2f257623
Daniel Lima <zatokiz@hotmail.com>
2021-06-24 23:25:21 -0300

    4° aula

--- src/route.js
+++ src/route.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require('express')
 
 const route = express.Router()
 
+// ******* ROTAS ********
 route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req, res) => res.render("room"))
+route.get('/create-pass', (req,res) => res.render("create-pass"))
+
 
 module.exports = route
